e95531d24d2c8fb4c1ec8cea196d53efebe0edd1
Julie <150014327+JuKa2023@users.noreply.github.com>
2024-04-16 16:49:44 +0200

    chnage layout Landingpage add image

--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -5,8 +5,8 @@ console.log('Hello World, this is the Landing Page');
 </script>
 
 <template>
-    <article>
-        <div>
+    <section class="content-container">
+        <div class="text-container">
             <h1>
                 Date Finder
             </h1>
@@ -17,18 +17,38 @@ console.log('Hello World, this is the Landing Page');
                 Möchtest du es ausprobieren?
             </h2>
             <button class="shadow-button">Click Me</button>
-        
-        <div>
-            <img src="/img/logo_datefinderr" alt="">
         </div>
+
+        <div class="image-content">
+            <img src="/" alt="">
         </div>
-    </article>
+    </section>
 </template>
 
 <style>
-    article{
+    .content-container {
         display: flex;
-        flex-direction: column;
+        justify-content: space-between; 
+        align-items: center; 
         max-width: 750px;
+        margin: 0 auto;
     }
+
+    .text-content {
+        flex: 1;
+        padding-right: 20px;
+    }
+
+    .image-content {
+        flex: 0 1 300px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    img {
+        max-width: 100%;
+        height: auto;
+    }
+
 </style>
