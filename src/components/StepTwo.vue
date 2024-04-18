1668183bd976f5199e72a149b072b01e023ecd05
Julie <150014327+JuKa2023@users.noreply.github.com>
2024-04-18 12:49:29 +0200

    create base layout for the stepper step 1 & 2

---
+++ src/components/StepTwo.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+      <h3>Was ist dein Budget?</h3>
+      <p>Bitte wähle dein maximales Budget aus, den du für deine Verabredung ausgeben möchtest.Es werden dir Vorschläge angezeigt, die deinem Budget entsprechen oder günstiger sind.</p>
+      <article v-for="option in options" :key="option.text" @click="selectOption(option)" class="cardNotClick">
+        <section>
+          <div>
+            <h2 class="activity">{{ option.text }}</h2>
+          </div>
+          <img class="Bildcard" :src="option.img" :alt="`illustration of ${option.text}`">
+          <img class="logo" src="/img/Platzhalter.png" alt="logo related to the pricing option">
+        </section>
+      </article>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        options: [
+          { text: 'Budgetfriendly', img: '/img/budgetfriendly.png' },
+          { text: 'Kostenlos', img: '/img/kostenlos.png' },
+          { text: 'Luxuriös', img: '/img/luxurious.png' }
+        ]
+      };
+    },
+    methods: {
+      selectOption(option) {
+        this.$emit('step-complete'); // Optionally pass option data
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  /* Your CSS here or in the global stylesheet */
+  </style>
+  
--- src/components/Stepper.vue
+++ src/components/Stepper.vue
@@ -1,13 +1,28 @@
 <template>
-    <article>
-        <section>
-
-        </section>
-        <section>
-
-        </section>
-        <section>
-            
-        </section>
-    </article>
+    <div>
+      <!-- Dynamically renders the current step component -->
+      <component :is="steps[currentStep].component" @step-complete="nextStep"/>
+    </div>
 </template>
+  
+<script>
+  export default {
+    data() {
+      return {
+        currentStep: 0,
+        steps: [
+          { component: 'StepOne' },
+          { component: 'StepTwo' }
+        ]
+      };
+    },
+    methods: {
+      nextStep() {
+        if (this.currentStep < this.steps.length - 1) {
+          this.currentStep++;
+        }
+      }
+    }
+  }
+</script>
+  
