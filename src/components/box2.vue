d57fa3878787a2e5ec0561a670d4178a3ab5abeb
Julie <150014327+JuKa2023@users.noreply.github.com>
2024-04-19 17:53:56 +0200

    added Styling to stepper

--- src/components/box2.vue
+++ src/components/box2.vue
@@ -11,50 +11,5 @@
 </template>
 
 <Style>
-    .cardNotClick{
-        background-color: #fefafd;
-        padding: 10px 20px;
-        margin: auto;
-        font-family: 'poppins bold', sans-serif;
-        max-width: 20%;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        border-radius: 10px;
-        box-shadow: 0 4px 8px 0px rgba(33, 18, 48, 1);
-    }
-
-.chips{
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    margin-top: 10px;
-}
-    .chip{
-        display: inline-block;
-        padding: 0 25px;
-        height: 40px;
-        font-size: 16px;
-        line-height: 40px;
-        border: #7A798C 1px solid;
-        border-radius: 25px;
-        background-color: #ffffff;
-        margin: 5px;
-        cursor: default;
-        color: #7A798C;
-    }
-    .activity{
-        margin-bottom: 0px;
-    }
-    .Bildcard{
-        margin: 10px;
-        max-width: 80%;
-    }
-
-    .logo{
-        margin: 10px;
-        max-width: 10%;
-    }
+    
 </Style>
