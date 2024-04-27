cbd290ab98dd6695e310dc4f6a98a70d725bb41e
Julie <150014327+JuKa2023@users.noreply.github.com>
2024-04-27 17:04:00 +0200

    WIP supabase RPC calls for date search

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,7 @@
 <script setup>
 import {onMounted, ref} from "vue";
 import {get_user} from "@/authentication";
+import {supabase} from "@/supabase";
 
 const name = ref('')
 
@@ -8,6 +9,21 @@ onMounted(async () => {
   console.log('Hello World, this is the Home Page')
   const user = await get_user();
   name.value = user.email
+
+  const tagsJson = {
+    tags: ['aktiv']
+  };
+
+  const { data, error } = await supabase
+      .rpc('get_ideas_by_tags_json', { input_json: tagsJson });
+
+  if (error) {
+    console.error('Error fetching ideas:', error);
+  } else {
+    console.log('Ideas:', data);
+  }
+
+
 })
 </script>
 
@@ -16,5 +32,8 @@ onMounted(async () => {
 </template>
 
 <style scoped>
+
+
+
 
 </style>
